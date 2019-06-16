4721bc08c4cc8eb2fd0cedfc9b47fc4ef79dc581
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-06-15 21:15:21 -0500

    heroku  9898347293

---
+++ client/build/precache-manifest.af7193298d7503956c6c0b7d77c7ec39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76f6dae34f9099fd87f7",
+    "url": "/static/css/main.96da9783.chunk.css"
+  },
+  {
+    "revision": "76f6dae34f9099fd87f7",
+    "url": "/static/js/main.864d61a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2484e323edd3207f407f",
+    "url": "/static/js/2.ba92f475.chunk.js"
+  },
+  {
+    "revision": "e97a53f0873ef58db82f2de32a73c67a",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bcb2ebb9ca1b64cef0cf770ecad6581.js"
+  "/precache-manifest.af7193298d7503956c6c0b7d77c7ec39.js"
 );
 
 workbox.clientsClaim();
